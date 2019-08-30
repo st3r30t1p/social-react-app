8977bab2bda51825fb9fb79610da527a74598576
lmr.artem.kolomoiets <lmr.artem.kolomoiets1@gmail.com>
2019-08-30 17:56:29 +0300

    added login redux-form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import {Field, reduxForm} from "redux-form";
 
-const Login = () => {
+let LoginForm = (props) => {
+
+    const {handleSubmit} = props;
     return (
-        <div className="app-content">
-            <h1>Login</h1>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <Field name="login" component="input" type="text" placeholder="Login"/>
+            </div>
+            <div>
+                <Field name="password" component="input" type="password" placeholder="Password"/>
+            </div>
+            <div>
+                <Field name="remember" component="input" type="checkbox" />
+                <label htmlFor="remember">Remember Me</label>
+            </div>
+            <button type="submit">Login</button>
+        </form>
     );
 };
+
+
+const Login = () => {
+
+    let handleSubmit = (values) => {
+        console.log(values);
+    };
+
+    return (
+    <div className="app-content">
+        <h1>Login</h1>
+        <LoginForm handleSubmit={handleSubmit} />
+    </div>
+    )
+};
+
+LoginForm = reduxForm({
+    // a unique name for the form
+    form: 'login'
+})(LoginForm);
 
 export default Login;
--- src/redux-store/redux-store.js
+++ src/redux-store/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from './reducers/profile-reducer';
 import dialogsReducer from './reducers/dialogs-reducer';
 import usersReducer from './reducers/users-reducer';
 import {authReducer} from "./reducers/auth-reducer";
-import reduxThunk from 'redux-thunk'
+import reduxThunk from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 let reducers = combineReducers({
     authData: authReducer,
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
+    form: formReducer
 });
 let store = createStore(reducers, applyMiddleware(reduxThunk));
 
